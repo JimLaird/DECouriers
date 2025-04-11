a2f40f70ea0673f41196c001e436c74b2fa1eee1
JimLaird <jimlaird01@gmail.com>
2025-04-11 15:02:50 +0100

    sorted issues with flyout layout on windows

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,15 +1,19 @@
-﻿namespace DECouriers
+﻿using Firebase.Auth;
+namespace DECouriers
 {
     public partial class App : Application
     {
-        public App()
+        private readonly FirebaseAuthClient _firebaseAuthClient;
+
+        public App(FirebaseAuthClient firebaseAuthClient)
         {
+            _firebaseAuthClient = firebaseAuthClient;
             InitializeComponent();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            return new Window(new AppShell(_firebaseAuthClient));
         }
     }
 }
